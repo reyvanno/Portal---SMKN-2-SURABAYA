92d523dfc31ffae6a4ab3bcdcc2a95c3447a7d5e
reyvanno <reyvannosandova@gmail.com>
2025-08-29 06:55:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-let lastScrollTop = 0;
-const mobileFooter = document.querySelector('footer.mobile');
+// let lastScrollTop = 0;
+// const mobileFooter = document.querySelector('footer.mobile');
 
-// window.addEventListener('scroll', () => {
-//     const st = window.pageYOffset || document.documentElement.scrollTop;
-//     if (st > lastScrollTop){
-//         mobileFooter.classList.add('hide'); 
-//         mobileFooter.classList.remove('hide');
-//     }
-//     lastScrollTop = st <= 0 ? 0 : st;
-// });
+// // window.addEventListener('scroll', () => {
+// //     const st = window.pageYOffset || document.documentElement.scrollTop;
+// //     if (st > lastScrollTop){
+// //         mobileFooter.classList.add('hide'); 
+// //         mobileFooter.classList.remove('hide');
+// //     }
+// //     lastScrollTop = st <= 0 ? 0 : st;
+// // });
